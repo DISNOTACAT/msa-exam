33468d3512ba9085e21ae508b5b4b734a31795af
DISNOTACAY <yeonjh915@gmail.com>
2024-12-03 07:52:51 +0900

    init project

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
@@ -0,0 +1,15 @@
+package com.sparta.msa_exam.order.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping
+    public String getOrder() {
+        return "Order details";
+    }
+}
